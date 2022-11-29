90552ab459cdd733ffb2278a691036ada69d5f4e
NackiE23 <dimavarchuc@gmail.com>
2022-11-29 18:03:41 +0200

    Cleaning

--- app/Services/Router.php
+++ app/Services/Router.php
@@ -10,18 +10,7 @@ class Router {
             "uri" => $uri,
             "page" => $page_name,
             "methods" => $methods,
-            "name" => $name,
-            "is_reg" => false
-        ];
-    }
-
-    public static function route_reg(string $uri, string $page_name, array $methods, string $name = null) {
-        self::$list[] = [
-            "uri" => $uri,
-            "page" => $page_name,
-            "methods" => $methods,
-            "name" => $name,
-            "is_reg" => true
+            "name" => $name
         ];
     }
 
@@ -45,14 +34,7 @@ class Router {
         $query = $_GET['q'];
         
         foreach(self::$list as $route) {
-            if ($route['is_reg']) {
-                if (in_array($_SERVER['REQUEST_METHOD'], $route['methods'])) {
-                    require_once "views/pages/" . $route['page'] . ".php";
-                    exit();
-                } else {
-                    break;
-                }
-            } elseif ($route['uri'] === '/' . $query) {
+            if ($route['uri'] === '/' . $query) {
                 if (in_array($_SERVER['REQUEST_METHOD'], $route['methods'])) {
                     require_once "views/pages/" . $route['page'] . ".php";
                     exit();
